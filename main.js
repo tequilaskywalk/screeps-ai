df5891401b06f0cb7d36588da76fc835370406eb
tequilaskywalk <mrsharp@gmx.de>
2016-06-30 11:08:04 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,39 @@
+// main.js
+
+// Initialize the game
+require('init');
+
+// Process the creeps
+var builder = require('builder');
+var guard = require('guard');
+var harvester = require('harvester');
+var healer = require('healer');
+for (var name in Game.creeps) {
+    var creep = Game.creeps[name];
+    switch (creep.memory.role) {
+        case 'builder':
+            builder(creep);
+            break;
+        case 'guard':
+            guard(creep);
+            break;
+        case 'harvester':
+            harvester(creep);
+            break;
+        case 'healer':
+            healer(creep);
+            break;
+        default:
+            // idle
+            break;
+    }
+}
+
+// Process the spawns
+var population = require('population');
+for(var name in Game.spawns) {
+    population(Game.spawns[name]);
+}
+
+// Cleanup dead objects
+require('garbagecollector');
